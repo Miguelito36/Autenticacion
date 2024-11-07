1f010445937882545fb07d3de83df43c1ea42b7f
Janeer Barrera <JaneerBn>
2024-11-06 20:19:34 -0500

    Primer Commit

--- Pages/Student/Student.cshtml.cs
+++ Pages/Student/Student.cshtml.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Autenticacion.Pages.Student
 {
+    [Authorize]
     public class StudentModel : PageModel
     {
         public void OnGet()
--- Pages/Teacher/Teacher.cshtml.cs
+++ Pages/Teacher/Teacher.cshtml.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Autenticacion.Pages.Teacher
 {
+    [Authorize]
     public class TeacherModel : PageModel
     {
         public void OnGet()
